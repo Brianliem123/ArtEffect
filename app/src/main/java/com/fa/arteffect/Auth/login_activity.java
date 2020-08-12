d91b8138d3ae38b2097e63740d977f73a02d959b
brian <febrianliem123@gmail.com>
2020-08-12 10:59:37 +0700

    Menambahakan XML Login dan beberapa dependencies

---
+++ app/src/main/java/com/fa/arteffect/Auth/login_activity.java
@@ -0,0 +1,16 @@
+package com.fa.arteffect.Auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fa.arteffect.R;
+
+public class login_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_activity);
+    }
+}
